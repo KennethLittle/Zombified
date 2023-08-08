d3c58db0f4e08d0126181c2891cd1a0d24a98e63
Peter02 <112404786+TrueSeer@users.noreply.github.com>
2023-08-07 20:39:32 -0400

    Player has mesh
    Made player have skin and put all scenes in 1 folder

--- Zombified/Assets/Scripts/playerController.cs
+++ Zombified/Assets/Scripts/playerController.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class playerController : MonoBehaviour, IDamage
 {
     [SerializeField] CharacterController controller;
+    //[SerializeField] Renderer model;
+    //[SerializeField] NavMeshAgent agent;
 
     [SerializeField] int HP;
     [SerializeField] float playerSpeed;
