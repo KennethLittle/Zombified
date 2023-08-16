185ae8bf002999182a0f02ba047d8f31d21b2e82
Richie Chao <110568947+fs-richie@users.noreply.github.com>
2023-08-15 23:58:17 -0700

    Redone Gun Audio & Minor Zombie Audio Edit
    • Redone Gun/Weapon Audio for the new inventory system
        • Added Audio SerializeFields to WeaponStats.cs
        • Added Audio to Assault Rifle and Pistol .asset
    
    Each individual gun will have their own shooting sfx, bullet casing drop sfx, and reload sfx
    
    • Edited the enemyAI.cs so that the voice playback "cooldown" min/max are SerializeFields

--- Zombified/Assets/Scripts/WeaponStats.cs
+++ Zombified/Assets/Scripts/WeaponStats.cs
@@ -10,6 +10,13 @@ public class WeaponStats : ScriptableObject
     public float shootDist;
     public int ammoCur;
     public int ammoMax;
+    
+    [SerializeField] public AudioClip[] audioShoot;
+    [SerializeField] [Range(0, 1)] public float audioShootVol;
+    [SerializeField] public AudioClip[] audioShootCasing;
+    [SerializeField] [Range(0, 1)] public float audioShootCasingVol;
+    [SerializeField] public AudioClip[] audioGunReload;
+    [SerializeField] [Range(0, 1)] public float audioGunReloadVol;
 
     public GameObject model;
     public ParticleSystem hitEffect;
