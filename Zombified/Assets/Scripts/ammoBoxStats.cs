696ca431913cd9f214e7e2d711e53f86e510ca2c
Rillo813 <robert.murillo.89@gmail.com>
2023-08-21 13:43:31 -0700

    Built a ammobox and ammo reload
    ammobox can be dragged onto the world scene from prefabs
    once picked up by the player (by walking over it) the UI will have a new number indicating the amount of ammo that they have to reload with
    Press R to reload ammo.
    This will not overfill guns. can be used with any currently equiped gun.
    Will not load past 0.

---
+++ Zombified/Assets/Scripts/ammoBoxStats.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ammoBoxStats : ScriptableObject
+{
+    public int ammoAmount;
+
+    public GameObject model;
+}
