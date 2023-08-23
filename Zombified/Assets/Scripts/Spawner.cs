9161dccf540079b4b7f8a107f03b2353eb80a358
Devan Little <KDLittle@student.fullsail.edu>
2023-08-23 17:08:59 -0500

    Some bug fixes.

--- Zombified/Assets/Scripts/Spawner.cs
+++ Zombified/Assets/Scripts/Spawner.cs
@@ -1,58 +1,58 @@
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class Spawner : MonoBehaviour
 {
-
     public GameObject Zombie3;
-    public Transform spawnPoint;
+    public Transform[] spawnPoints; // Array of spawn positions
+    public float timeBetweenZombieSpawns;
     public int startingZombies;
     public int minAdditionalZombies;
     public int maxAdditionalZombies;
-    public int activationWave = 1; // The wave number at which this spawner is activated
+    public int activationWave;
     [Range(1, 5)][SerializeField] private float enemyHPMultiplier = 2.0f;
     [Range(1, 5)][SerializeField] private float enemyDamageMultiplier = 1.5f;
     private WaveManager waveManager;
 
-
     private void Start()
     {
-        waveManager = GetComponentInParent<WaveManager>();
+        waveManager = WaveManager.instance;
     }
 
     public void SpawnWave()
     {
-        int numZombies = startingZombies;
+        if (waveManager.waveNumber < activationWave || spawnPoints.Length == 0) return;
 
-        if (waveManager.waveNumber > 1)
-        {
-            numZombies += waveManager.waveNumber * Random.Range(minAdditionalZombies, maxAdditionalZombies + 1);
-        }
-
+        int numZombies = startingZombies + waveManager.waveNumber * Random.Range(minAdditionalZombies, maxAdditionalZombies + 1);
         waveManager.enemiesRemaining += numZombies;
-        gameManager.instance.waveNumberText.text = "Wave " +(waveManager.waveNumber);
+        gameManager.instance.waveNumberText.text = "Wave " + waveManager.waveNumber;
 
+        StartCoroutine(SpawnZombies(numZombies));
+    }
+
+    private IEnumerator SpawnZombies(int numZombies)
+    {
         for (int i = 0; i < numZombies; i++)
         {
             SpawnZombie();
+            yield return new WaitForSeconds(timeBetweenZombieSpawns);
         }
     }
 
     private void SpawnZombie()
     {
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)]; // Choose a random spawn point
         GameObject newZombie = Instantiate(Zombie3, spawnPoint.position, spawnPoint.rotation);
         enemyAI zombieAI = newZombie.GetComponent<enemyAI>();
+
+        int baseDamage = zombieAI.damage;
+        int baseHP = zombieAI.HP;
 
         int damageMultiplier = Mathf.RoundToInt(Mathf.Pow(enemyDamageMultiplier, waveManager.waveNumber - 1));
         int hpMultiplier = Mathf.RoundToInt(Mathf.Pow(enemyHPMultiplier, waveManager.waveNumber - 1));
 
-        zombieAI.damage = damageMultiplier;
-        zombieAI.HP = hpMultiplier;
+        zombieAI.damage = baseDamage * damageMultiplier;
+        zombieAI.HP = baseHP * hpMultiplier;
         zombieAI.spawnPoint = spawnPoint;
-    }
-
-    public void SetWaveManager(WaveManager waveManager)
-    {
-        this.waveManager = waveManager;
     }
 }
