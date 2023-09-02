8509b7120f27b784770073164375a6f9699a17aa
Rillo813 <robert.murillo.89@gmail.com>
2023-09-02 12:50:52 -0700

    Made the bare bones for a playerInteractor and NPCInteractable scripts.
    Right now they are tested by calling "something" to console when the player is in range of the NPC collider and presses E.

---
+++ Zombified/Assets/Scripts/playerInteract.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerInteract : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            float interactRange = 2f;
+            Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
+            foreach (Collider collider in colliderArray)
+            {
+                if (collider.TryGetComponent(out NPCInteractable npcInteractable))
+                {
+                    npcInteractable.Interact();
+                }
+            }
+        }
+    }
+    }
+
