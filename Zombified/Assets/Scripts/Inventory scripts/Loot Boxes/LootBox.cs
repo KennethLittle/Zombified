49a7f1b7f6e50510b98c4a65c36149ae06c154b4
Jadeyn08 <JCFisher4@student.fullsail.edu>
2023-09-07 20:20:55 -0500

    Loot Box Update
    updated drop chances

--- Zombified/Assets/Scripts/Inventory scripts/Loot Boxes/LootBox.cs
+++ Zombified/Assets/Scripts/Inventory scripts/Loot Boxes/LootBox.cs
@@ -1,28 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 
 public class LootBox : MonoBehaviour
 {
-    public List<ScriptableObject> LootList = new List<ScriptableObject>();
+    public ItemManager itemManager;
 
     private void Start()
     {
 
+        droppedLoot();
+
     }
     List<ScriptableObject> droppedLoot()
     {
-        int random = Random.Range(0, 101);
+        int amountDropped = Random.Range(1, 5);
+
         List<ScriptableObject> droppingLoot = new List<ScriptableObject>();
 
-        foreach (ScriptableObject loot in LootList)
+        for (int i = 0; i < amountDropped; i++)
         {
-            if (random <= 15)
+            int dropChance = Random.Range(1, 101);
+
+            if (dropChance <= 15)
             {
-
+                medPackStats med = itemManager.GetItem<medPackStats>("MedPack");
+                droppingLoot.Add(med);
             }
-
+            else if(dropChance <= 25)
+            {
+                ammoBoxStats ammo = itemManager.GetItem<ammoBoxStats>("Ammo");
+                droppingLoot.Add(ammo);
+            }
+            else if (dropChance <= 30)
+            {
+                WeaponStats snipe = itemManager.GetItem<WeaponStats>("Sniper Rifle");
+                droppingLoot.Add(snipe);
+            }
+            else if (dropChance <= 40)
+            {
+                WeaponStats shotgun = itemManager.GetItem<WeaponStats>("Shotgun");
+                droppingLoot.Add(shotgun);
+            }
+            else if (dropChance <= 50)
+            {
+                WeaponStats assualtRifle = itemManager.GetItem<WeaponStats>("Assualt Rifle");
+                droppingLoot.Add(assualtRifle);
+            }
+            else if (dropChance <= 60)
+            {
+                WeaponStats ak = itemManager.GetItem<WeaponStats>("AK-47");
+                droppingLoot.Add(ak);
+            }
         }
         return droppingLoot;
     }
