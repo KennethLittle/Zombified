23ed6bdb7d16ae88a580f61c90045d05598f4b51
Andre A. Green <agreen1@student.fullsail.edu>
2023-09-14 20:39:49 -0400

    Update DialogueManager.cs

--- Zombified/Assets/Scripts/Quests/DialogueManager.cs
+++ Zombified/Assets/Scripts/Quests/DialogueManager.cs
@@ -14,4 +14,43 @@ public class DialogueManager : MonoBehaviour
         sentences = new Queue<string>();
     }
 
+    public void StartDialogue(Dialogue dialogue)
+    {
+        nameText.text = dialogue.speakerName;
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+            sentences.Enqueue(sentence);
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0) 
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines(); // Ensure the text isn't still typing out when we get the next sentence
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null; // This gives the typewriter effect. Adjust as needed.
+        }
+    }
+
+    void EndDialogue()
+    {
+        // Logic for when dialogue ends.
+        // Maybe hide the dialogue box or signal another system.
+    }
 }
